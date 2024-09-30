b62e00b46229ceed4339bc3197e3879cda36db31
sixzjkk <jenifergmporfirio@gmail.com>
2024-09-30 17:33:26 -0400

    aula 30/09

--- src/exemplo2/index.jsx
+++ src/exemplo2/index.jsx
@@ -2,23 +2,30 @@ import { useEffect, useState } from "react";
 
 export default function App() {
   
-  const [??,??] = useState([]);
+  const [fotos,setFotos] = useState([]);
 
   useEffect(() => { 
 
     const buscarUsuario  = async () => {
-                    = await fetch('https://jsonplaceholder.typicode.com/photos');
-        const dados =                ;
+        const resposta = await fetch('https://jsonplaceholder.typicode.com/photos');
+        const dados = await resposta.json();
         setFotos(dados);
     }
     buscarUsuario();
-  }, //complete o código);
+  }, []);
+
 
   return (
     <>
       <h1>Galeria de Fotos</h1>
       <ul>
-        {}
+        {fotos.map(produto => (
+          <li key={produto.id}>
+            <h2>{produto.title}</h2>
+            <p>{produto.albumId}</p>
+            <img src={produto.url} alt={produto.title} width={100} />
+          </li>
+        ))}
       </ul>
     </>
   );
